73a53ec9c89fe8f99162addb957e197973aeb2df
evansimoni <evan.simoni@gmail.com>
2014-08-19 21:26:23 -0700

    sets up a module to wrap API calls in methods; currently broken, not accessible in the controller

---
+++ app/models/brew_bar_baz.rb
@@ -0,0 +1,17 @@
+# module Brewskis
+
+#   class Client
+#     include HTTParty
+
+#     base_uri "http://api.brewerydb.com/v2/?key=ef1069aca2f731647326ccfbb58a47d4/"
+
+#     def initialize
+#     end
+
+#     def test_method
+#       response = JSON.parse(self.class.get('/beer/oeGSxs'))
+#       return response
+#     end
+
+#   end
+# end
