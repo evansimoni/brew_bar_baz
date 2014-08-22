c3fc4cb1cb013cf6a2fa56ee2a5b1389e10f8b2c
evansimoni <evan.simoni@gmail.com>
2014-08-21 17:41:51 -0700

    adds ability to have brewery photos, breaks map load :(

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,10 +4,17 @@ end
 
 post '/' do
   api = Brewskis::Client.new(params)
-  @breweries = api.find_locations
-  @breweries = @breweries.parsed_response["data"]
+  brewskis = api.find_locations
+  # p brewskis
+  @breweries = brewskis.parsed_response["data"]
   p @breweries
+  p "===================="
+  p @breweries[1]["brewery"]["images"]["icon"]
+  p "===================="
   erb :index
 end
 
 # grabs params for zipcode
+post '/beers' do
+
+end
